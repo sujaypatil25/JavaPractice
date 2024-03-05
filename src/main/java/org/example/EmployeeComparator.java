70253e489fdb6e420538df7e0146c406c347a805
Sujay Patil <patil.sujay05@gmail.com>
2024-03-05 12:13:52 +0530

    Example of Comparator Implementation

--- src/main/java/org/example/EmployeeComparator.java
+++ src/main/java/org/example/EmployeeComparator.java
@@ -5,6 +5,6 @@ import java.util.Comparator;
 public class EmployeeComparator implements Comparator<Employee> {
     @Override
     public int compare(Employee o1, Employee o2) {
-        return o1.geteName().compareTo(o2.geteAddress());
+        return o1.geteAddress().compareTo(o2.geteAddress());
     }
 }
